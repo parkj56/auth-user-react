b01082a9fdc5ac09a6eaa2997a169c77b145ef94
PNieves30 <89657437+parkj56@users.noreply.github.com>
2023-11-06 15:57:46 -0600

    initializing states for username, password, and email. Importing axios for making requests to backend.

---
+++ user-auth-react/src/Signup.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import "./Login.scss"
+import axios from 'axios';
+
+const SignUp = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+}
